6432f0d387d99aef8ce7c1d08576eeca6cb3308f
Gergő Papp-Szentannai <gergo_13@me.com>
2017-05-27 22:00:05 +0300

    Add new image. Remove edge and line detection. Resize windows. Some more refactoring.

--- src/DartsDetector.cpp
+++ src/DartsDetector.cpp
@@ -17,6 +17,39 @@
 #include "Mask.h"
 #include "ImFill.h"
 
+int width = 500;
+int height = 500;
+
+int dist(const cv::Point& a, const cv::Point& b)
+{
+	return int(sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2)));
+}
+
+/*std::vector<cv::Vec<int, 4>> filterLines(const std::vector<cv::Vec<int, 4>>&  lines, cv::Point center, const Mask& mask)
+{
+	std::vector<cv::Vec<int, 4>> filteredLines;
+	int rectSize = 20;
+	cv::Rect_<int> centerRectangle = cv::Rect_<int>(center.x - rectSize / 2, center.y - rectSize / 2, rectSize, rectSize);
+	cv::Vec<int, 4> referenceLine;
+	int maxDist = -1;
+
+	for (auto line : lines)
+	{
+		cv::Point a(line[0], line[1]);
+		cv::Point b(line[2], line[3]);
+
+		if (dist(a, b) > maxDist)
+		{
+			maxDist = dist(a, b);
+			referenceLine = line;
+		}
+	}
+
+	filteredLines.push_back(referenceLine);
+
+	return filteredLines;
+}*/
+
 int main()
 {
 	bool showImages = true;
@@ -28,13 +61,21 @@ int main()
 	// Read images:
 
 	std::cout << "Reading background image.\n";
-	cv::Mat backgroundImage = imread("Images/background.bmp", cv::IMREAD_COLOR);
+	cv::Mat backgroundImage = imread("Images/DartHigh.jpg", cv::IMREAD_COLOR);
+	cv::GaussianBlur(backgroundImage, backgroundImage, cv::Size(3, 3), 1, 0);
 
 	std::cout << "Reading Dart Image.\n";
-	cv::Mat dartImage = imread("Images/dart.bmp", cv::IMREAD_COLOR);
+	cv::Mat dartImage = imread("Images/DartHigh.jpg", cv::IMREAD_COLOR);
+	cv::GaussianBlur(dartImage, dartImage, cv::Size(3, 3), 1, 0);
 
 	if (showImages)
 	{
+		namedWindow("Background", cv::WINDOW_NORMAL);
+		namedWindow("Dart", cv::WINDOW_NORMAL);
+
+		cv::resizeWindow("Background", width, height);
+		cv::resizeWindow("Dart", width, height);
+
 		imshow("Background", backgroundImage);
 		imshow("Dart", dartImage);
 	}
@@ -47,11 +88,17 @@ int main()
 	cvtColor(backgroundImage, grayBackgroundImage, cv::COLOR_RGB2GRAY);
 
 	cv::Mat BWBackgroundImage;
-	auto thresholdValue = 75; // set by trial and error
+	auto thresholdValue = 90; // set by trial and error
 	threshold(grayBackgroundImage, BWBackgroundImage, thresholdValue, 255, cv::THRESH_BINARY_INV);
 
 	if (showImages)
 	{
+		namedWindow("Gray background imaged", cv::WINDOW_NORMAL);
+		namedWindow("BW background image", cv::WINDOW_NORMAL);
+
+		cv::resizeWindow("Gray background imaged", width, height);
+		cv::resizeWindow("BW background image", width, height);
+
 		imshow("Gray background image", grayBackgroundImage);
 		imshow("BW background image", BWBackgroundImage);
 	}
@@ -63,6 +110,10 @@ int main()
 
 	if (showImages)
 	{
+		namedWindow("Mask background", cv::WINDOW_NORMAL);
+
+		cv::resizeWindow("Mask background", width, height);
+
 		imshow("Mask background", maskBackground);
 	}
 
@@ -71,23 +122,58 @@ int main()
 	// TODO: crop images - only increases performance, not needed now
 
 
-	// Create Point map
+	// Create Point map:
 
 	std::cout << "Creating point map.\n";
 	Mask mask(backgroundImage);
 
-	// Center
+	// Center:
 
 	std::vector<cv::Point> points;
 	cv::Point center;
 	findNonZero(mask.innerBull, points);
 	for (auto point : points)
 		center += point;
-	center.x /= points.size();
-	center.y /= points.size();
+	center.x /= int(points.size());
+	center.y /= int(points.size());
 
 	std::cout << "Center: (" << center.x << ", " << center.y << ").\n";
 
+/*	// Edge detection:
+
+	cv::Mat edgeImage;
+	cv::Canny(grayBackgroundImage & mask.board, edgeImage, 150, 200);
+	morphologyEx(edgeImage, edgeImage, CV_MOP_CLOSE, getStructuringElement(cv::MORPH_CROSS, cv::Size(3, 3)), CvPoint(-1, -1), 3);
+
+	// Straight line detection:
+
+	cv::Mat linesImage = backgroundImage.clone();
+
+
+	std::vector<cv::Vec4i> lines;
+	cv::HoughLinesP(edgeImage, lines, 1, CV_PI / 180, 90, 150, 15);
+	lines = filterLines(lines, center, mask);
+	for (size_t i = 0; i < lines.size(); i++)
+	{
+		line(linesImage, cv::Point(lines[i][0], lines[i][1]),
+		     cv::Point(lines[i][2], lines[i][3]), cv::Scalar(0, 0, 255), 3, 8);
+	}
+	line(linesImage, cv::Point(center.x, center.y),
+		cv::Point(center.x, center.y), cv::Scalar(255, 0, 0), 3, 8);
+	
+
+	if (showImages)
+	{
+		namedWindow("Edges", cv::WINDOW_NORMAL);
+		namedWindow("Lines", cv::WINDOW_NORMAL);
+
+		cv::resizeWindow("Edges", width, height);
+		cv::resizeWindow("Lines", width, height);
+
+		imshow("Edges", edgeImage);
+		imshow("Lines", linesImage);
+	}*/
+	
 
 	// Finish
 
